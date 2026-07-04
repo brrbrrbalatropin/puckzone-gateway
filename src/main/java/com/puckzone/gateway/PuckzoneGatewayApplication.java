3f695f065066577069007311264a2f512c09c831
Daniel Felipe Rayo Rodriguez <daniel.rayo@mail.escuelaing.edu.co>
2026-07-03 22:16:10 -0500

    first commit

---
+++ src/main/java/com/puckzone/gateway/PuckzoneGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.puckzone.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuckzoneGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuckzoneGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/puckzone/gateway/PuckzoneGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puckzone.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuckzoneGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
